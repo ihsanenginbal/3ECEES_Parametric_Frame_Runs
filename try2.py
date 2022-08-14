a90172dcee76cbe1aac3c31678eec65449019f1d
İhsan Engin Bal <38361052+ihsanenginbal@users.noreply.github.com>
2022-08-14 11:05:54 +0300

    Update try2.py

--- try2.py
+++ try2.py
@@ -5,8 +5,8 @@ rank = MPI.COMM_WORLD.Get_rank()
 a = 6.0
 b = 3.0
 if rank == 0:
-        print a + b
+        print(a + b)
 if rank == 1:
-        print a * b
+        print(a * b)
 if rank == 2:
         print max(a,b)
